a2e170bb58a15bc31431ef24bb3ad5e16e3e2daa
Camila <itsmyllaa@github.com>
2023-03-13 12:22:10 -0300

    Trilha de Kanban

---
+++ kanban/Introdução/kanban.ts
@@ -0,0 +1,13 @@
+/* O Kanban nasceu na Toyota após o final da segunda guerra, após a visita de seu idealizador Taiichi a um supermercado nos EUA. 
+Ele gostou da maneira simples com que os supermercadps faziam o abastecimento das gondolas através dos repositores, e decidiu adaptar ao controle dos estoques na fábrica.
+Aplicação no desenvolvimento de software.
+David J. Anderson utilizou o método Kanban dentro da Corbis (Empresa fundada por Bill Gates).
+Limitar o work in progress.
+Trelo
+Jira
+Task Office Planner
+Notion
+Todoist
+Documente! Realize uma boa documentação para gerir e medir o que é realizado.
+Extreme Ownership: Livro que aborda sobre os processos de decições dos bons líderes, escrito por um dos SEALS.
+"Esses líderes não apontavam a culpa. Eles não tinham desculpas. Em vez de reclamar sobre os desafios ou contratempos, eles criavam soluções e resolviam problemas. Ele otimizavam ativos, relacionamentos e insumos para fazer o que precisava ser feito. Seus próprios egos ficavam de lado em relação à missão e suas tropas. Esses líderes realmente lideravam."*/
